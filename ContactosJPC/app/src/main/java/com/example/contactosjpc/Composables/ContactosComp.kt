60d779638851da2c5639a7b5fa15b0b5d633ceee
Lorenzo <lorenzo.bellido.b@gmail.com>
2024-10-12 20:24:30 +0200

    terminado

--- ContactosJPC/app/src/main/java/com/example/contactosjpc/Composables/ContactosComp.kt
+++ ContactosJPC/app/src/main/java/com/example/contactosjpc/Composables/ContactosComp.kt
@@ -1,5 +1,6 @@
 package com.example.contactosjpc.Composables
 
+import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -13,6 +14,10 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
@@ -33,6 +38,8 @@ fun ItemList(itemContacto: List<Contacto>) {
 
 @Composable
 fun ContactoView(contacto: Contacto) {
+    var mostrar by rememberSaveable { mutableStateOf(false) }
+    Log.d(":::Contactos", mostrar.toString())
     var foto = R.drawable.men
     if (contacto.sexo == 1) {
         foto = R.drawable.women
@@ -47,20 +54,31 @@ fun ContactoView(contacto: Contacto) {
                 )
             }
             Column {
-                TextButton(
-                    text = contacto.name.substring(1)
-                ) { }
+                Log.d(":::Contactos", mostrar.toString())
 
-                Text(
-                    text = contacto.name,
-                    fontSize = 24.sp,
-                    modifier = Modifier.padding(8.dp)
-                )
-                Text(
-                    text = contacto.tfno,
-                    fontSize = 24.sp,
-                    modifier = Modifier.padding(8.dp)
-                )
+                if (mostrar) {
+                    Log.d(":::Contactos", mostrar.toString())
+                    Text(
+                        text = contacto.name,
+                        fontSize = 24.sp,
+                        modifier = Modifier.padding(8.dp)
+                    )
+                    Text(
+                        text = contacto.tfno,
+                        fontSize = 24.sp,
+                        modifier = Modifier.padding(8.dp)
+                    )
+                } else {
+                    TextButton(
+                        onClick = { mostrar = true }
+                    ) {
+                        val iniciales: String =
+                            contacto.name.uppercase().subSequence(0, 2).toString()
+                        Text(iniciales, fontSize = 50.sp)
+                        Log.d(":::Contactos", mostrar.toString())
+                    }
+
+                }
             }
         }
     }
