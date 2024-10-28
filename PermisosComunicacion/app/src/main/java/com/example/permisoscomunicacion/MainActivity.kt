91edc02d42450301bb21067491e44888d3b2bfa3
LorenzoBellidoB <lorenzo.bellido.b@gmail.com>
2024-10-28 12:49:17 +0100

    comit

---
+++ PermisosComunicacion/app/src/main/java/com/example/permisoscomunicacion/MainActivity.kt
@@ -0,0 +1,83 @@
+package com.example.permisoscomunicacion
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.net.NetworkCapabilities
+import android.net.wifi.WifiManager
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import com.example.permisoscomunicacion.ui.theme.PermisosComunicacionTheme
+import android.net.ConnectivityManager as ConnectivityManager1
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            PermisosComunicacionTheme {
+                var context: Context = LocalContext.current.applicationContext
+
+
+
+                Inicio(context)
+
+            }
+        }
+
+    }
+
+    @Composable
+    fun Inicio(context: Context){
+        Column {
+            Wifi(context)
+            Red(context)
+            Ubicacion(context)
+            Nfc(context)
+            Bluetooth(context)
+        }
+    }
+
+    private fun wifiActivo(context: Context): Boolean {
+        val wifiManager = context.getSystemService(Context.WIFI_SERVICE) as WifiManager
+        return wifiManager.isWifiEnabled
+    }
+
+    @Composable
+    fun Wifi(context: Context){
+
+        if(wifiActivo(context)){
+            Text(text = "WIFI",
+                Modifier.background(Color.Green))
+        }else{
+            Text(text = "WIFI",
+                Modifier.background(Color.Red))
+        }
+
+    }
+    @Composable
+    fun Red(context: Context){
+        Text(text = "Internet")
+    }
+    @Composable
+    fun Ubicacion(context: Context){
+        Text(text = "GPS")
+    }
+    @Composable
+    fun Nfc(context: Context){
+        Text(text = "NFC")
+    }
+    @Composable
+    fun Bluetooth(context: Context){
+        Text(text = "Bluetooth")
+    }
+}
+
