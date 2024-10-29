45e6fb663403a0f8a6bfb8a5a92e16db2498e2a6
LorenzoBellidoB <lorenzo.bellido.b@gmail.com>
2024-10-29 08:30:00 +0100

    comitted

--- PermisosComunicacion/app/src/main/java/com/example/permisoscomunicacion/MainActivity.kt
+++ PermisosComunicacion/app/src/main/java/com/example/permisoscomunicacion/MainActivity.kt
@@ -1,9 +1,10 @@
 package com.example.permisoscomunicacion
 
-import android.annotation.SuppressLint
 import android.bluetooth.BluetoothManager
 import android.content.Context
+import android.content.Context.CONNECTIVITY_SERVICE
 import android.location.LocationManager
+import android.net.ConnectivityManager
 import android.net.NetworkCapabilities
 import android.net.wifi.WifiManager
 import android.nfc.NfcAdapter
@@ -12,14 +13,23 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 import com.example.permisoscomunicacion.ui.theme.PermisosComunicacionTheme
-import android.net.ConnectivityManager as ConnectivityManager1
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,107 +37,126 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             PermisosComunicacionTheme {
+
                 var context: Context = LocalContext.current.applicationContext
 
-                Inicio(context)
 
+
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Inicio(
+                        modifier = Modifier.padding(innerPadding).height(50.dp).size(50.dp),
+                        context
+                    )
+                }
+            }
+        }
+    }
+}
+
+fun wifiFunciona(context: Context){
+
+    val connectivityManager = context.getSystemService(CONNECTIVITY_SERVICE) as ConnectivityManager
+    val networkCapabilities = connectivityManager.getNetworkCapabilities(
+        connectivityManager.activeNetwork
+    )
+    val isConnected = networkCapabilities?.hasCapability(
+        NetworkCapabilities.NET_CAPABILITY_INTERNET
+    ) == true
+}
+
+fun wifiActivo(context: Context): Boolean {
+    val wifiManager = context.getSystemService(Context.WIFI_SERVICE) as WifiManager
+    return wifiManager.isWifiEnabled
+}
+
+fun bluetoothActivo(context: Context): Boolean {
+    val bluetoothManager = context.getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
+    val bluetoothAdapter = bluetoothManager.adapter
+
+    return bluetoothAdapter != null && bluetoothAdapter.isEnabled
+}
+
+fun NFCActivo(context: Context): Boolean {
+    val nfcAdapter: NfcAdapter? = NfcAdapter.getDefaultAdapter(context)
+    return nfcAdapter != null && nfcAdapter.isEnabled
+}
+
+fun GPSActivo(context: Context): Boolean {
+    val locationManager = context.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+    return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+}
+
+@Composable
+fun Inicio(modifier: Modifier = Modifier, context: Context) {
+    Column(horizontalAlignment = Alignment.CenterHorizontally){
+
+        Spacer(Modifier.size(10.dp))
+
+        Row(){
+            Text(
+                text = "Wifi:"
+            )
+            if(wifiActivo(context)) {
+                Box(
+                    modifier.background(Color.Green)
+                )
+            }else{
+                Box(
+                    modifier.background(Color.Red)
+                )
             }
         }
 
-    }
+        Spacer(Modifier.size(10.dp))
 
-    @Composable
-    fun Inicio(context: Context){
-        Column {
-            Wifi(context)
-            /*Red(context)
-            Ubicacion(context)
-            Nfc(context)
-            Bluetooth(context)*/
-        }
-    }
-
-    fun WifiActivo(context: Context): Boolean {
-        val wifiManager = context.getSystemService(Context.WIFI_SERVICE) as WifiManager
-        return wifiManager.isWifiEnabled
-    }
-
-    /*fun RedActivo(context: Context): Boolean {
-        val redManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as WifiManager
-        return redManager.isWifiEnabled
-    }
-
-    fun UbicacionActivo(context: Context): Boolean {
-        val locationManager = context.getSystemService(Context.LOCATION_SERVICE) as LocationManager
-        val isGPSEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
-        return isGPSEnabled
-    }
-
-    fun NfcActivo(context: Context): Boolean {
-        val nfcAdapter: NfcAdapter? = NfcAdapter.getDefaultAdapter(context)
-        return nfcAdapter != null && nfcAdapter.isEnabled
-
-    }
-
-    fun BluetoothActivo(context: Context): Boolean {
-        val bluetoothManager = context.getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
-        val bluetoothAdapter = bluetoothManager.adapter
-        return bluetoothAdapter != null && bluetoothAdapter.isEnabled
-
-    }*/
-
-    @Composable
-    fun Wifi(context: Context){
-
-        if(WifiActivo(context)){
-            Text(text = "WIFI",
-                Modifier.background(Color.Green))
-        }else{
-            Text(text = "WIFI",
-                Modifier.background(Color.Red))
+        Row(){
+            Text(
+                text = "Bluetooth:"
+            )
+            if(bluetoothActivo(context)) {
+                Box(
+                    modifier.background(Color.Green)
+                )
+            }else{
+                Box(
+                    modifier.background(Color.Red)
+                )
+            }
         }
 
-    }
-    /*@Composable
-    fun Red(context: Context){
-        if(RedActivo(context)){
-            Text(text = "WIFI",
-                Modifier.background(Color.Green))
-        }else{
-            Text(text = "WIFI",
-                Modifier.background(Color.Red))
-        }
-    }
+        Spacer(Modifier.size(10.dp))
 
-    @Composable
-    fun Ubicacion(context: Context){
-        if(UbicacionActivo(context)){
-            Text(text = "GPS",
-                Modifier.background(Color.Green))
-        }else{
-            Text(text = "GPS",
-                Modifier.background(Color.Red))
+        Row(){
+            Text(
+                text = "NFC:"
+            )
+            if(NFCActivo(context)) {
+                Box(
+                    modifier.background(Color.Green)
+                )
+            }else{
+                Box(
+                    modifier.background(Color.Red)
+                )
+            }
+        }
+
+        Spacer(Modifier.size(10.dp))
+
+        Row(){
+            Text(
+                text = "GPS:"
+            )
+            if(GPSActivo(context)) {
+                Box(
+                    modifier.background(Color.Green)
+                )
+            }else{
+                Box(
+                    modifier.background(Color.Red)
+                )
+            }
         }
     }
-    @Composable
-    fun Nfc(context: Context){
-        if(NfcActivo(context)){
-            Text(text = "NFC",
-                Modifier.background(Color.Green))
-        }else{
-            Text(text = "NFC",
-                Modifier.background(Color.Red))
-        }
-    }
-    @Composable
-    fun Bluetooth(context: Context){
-        if(BluetoothActivo(context)){
-            Text(text = "Bluetooth",
-                Modifier.background(Color.Green))
-        }else{
-            Text(text = "Bluetooth",
-                Modifier.background(Color.Red))
-        }
-    }*/
 }
 
