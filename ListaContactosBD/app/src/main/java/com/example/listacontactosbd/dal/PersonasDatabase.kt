58fbbb6b7bf8b3d43dcd97564ff963389d7d282c
LorenzoBellido <145675169+LorenzoBellido@users.noreply.github.com>
2024-11-18 20:44:32 +0100

    acabado

--- ListaContactosBD/app/src/main/java/com/example/listacontactosbd/dal/PersonasDatabase.kt
+++
@@ -1,9 +0,0 @@
-package com.example.listacontactosbd.dal
-
-import androidx.room.Database
-import androidx.room.RoomDatabase
-
-@Database(entities = [PersonaEntity::class], version = 1)
-abstract class PersonasDatabase: RoomDatabase() {
-    abstract fun personaDao(): PersonasDao
-}
